214606a01f0bf5d18abd977a7181a9ee0bf501bc
Jesse Showalter <iamjesseshowalter@gmail.com>
2014-11-20 23:55:03 -1000

    Simple color change
    Menu nav area changed using new variable found in variables partial.
    Also replaced button color for transparent for possible future changes.

---
+++ build/js/map.js
@@ -0,0 +1,1 @@
+function initialize(){{var e=document.getElementById("map_canvas"),a={center:new google.maps.LatLng(44.5403,-78.5463),zoom:8,mapTypeId:google.maps.MapTypeId.ROADMAP};new google.maps.Map(e,a)}}google.maps.event.addDomListener(window,"load",initialize);
